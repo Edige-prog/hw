637011e95c6386cf9bfcf5834ca2bbe26dc63d08
Edige-prog <akimali.edige@gmail.com>
2025-03-21 14:13:33 +0500

    db_url

--- hw7/app/database/db.py
+++ hw7/app/database/db.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://store_user:SomePassword123@localhost/flowers_db"
+
+# "postgresql+psycopg2://store_user:SomePassword123@localhost/flowers_db"
+SQLALCHEMY_DATABASE_URL = os.environ['DB_URL']
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -17,50 +20,3 @@ def get_db():
 
 def init_tables():
     Base.metadata.create_all(bind=engine)
-
-# import psycopg2
-# from psycopg2.extras import RealDictCursor
-#
-# def get_connection():
-#     return psycopg2.connect(
-#         host="localhost",
-#         dbname="flowers_db",
-#         user="store_user",
-#         password="SomePassword123"
-#     )
-
-# def init_tables():
-#     with get_connection() as conn:
-#         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-#             cur.execute("""
-#                 CREATE TABLE IF NOT EXISTS users (
-#                     id SERIAL PRIMARY KEY,
-#                     email TEXT UNIQUE NOT NULL,
-#                     full_name TEXT NOT NULL,
-#                     password_hash TEXT NOT NULL,
-#                     photo_url TEXT
-#                 );
-#             """)
-#             cur.execute("""
-#                 CREATE TABLE IF NOT EXISTS flowers (
-#                     id SERIAL PRIMARY KEY,
-#                     name TEXT NOT NULL,
-#                     quantity INT,
-#                     price NUMERIC
-#                 );
-#             """)
-#             cur.execute("""
-#                 CREATE TABLE IF NOT EXISTS purchase (
-#                     id SERIAL PRIMARY KEY,
-#                     user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE
-#                 );
-#             """)
-#             cur.execute("""
-#                 CREATE TABLE IF NOT EXISTS purchase_item (
-#                     id SERIAL PRIMARY KEY,
-#                     purchase_id INT NOT NULL REFERENCES purchase(id) ON DELETE CASCADE,
-#                     flower_id INT NOT NULL REFERENCES flowers(id) ON DELETE CASCADE,
-#                     quantity INT
-#                 );
-#             """)
-#             conn.commit()  # ✅ Commits changes
